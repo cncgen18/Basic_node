d878f1492e74cc4bce6207494cac4a27690c0598
Michael Weyman <mweyman@combinedpublic.com>
2014-11-18 11:06:27 -0500

    added good and another route
    weird server printout

--- app.js
+++ app.js
@@ -15,7 +15,31 @@ server.route({
         reply('hello world');
     }
 });
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.pack.register({
+    plugin: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            args:[{ log: '*', request: '*' }]
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+
 // Start the server
-server.start(function () {
-    console.log('Server running at:', server.info.uri);
+	server.start(function () {
+    	server.log('info', 'Server running at:', server.info.uri);
+	});
+
 });
