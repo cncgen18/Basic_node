e363cd5656214d447fe7f4f0266775aa7262032c
Michael Weyman <mweyman@combinedpublic.com>
2014-11-18 10:51:47 -0500

    added server log and good
    also changed path to '/' instead of '/hello'

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 var Hapi = require('hapi');
+var Good = require('good');
 
 // Create a server with a host and port
 var server = new Hapi.Server('localhost', 8000);
 
+
+
 // Add the route
 server.route({
     method: 'GET',
-    path: '/hello',
+    path: '/',
     handler: function (request, reply) {
 
         reply('hello world');
     }
 });
-
+server.pack.register({
+    plugin: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            args:[{ log: '*', request: '*' }]
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
 // Start the server
-server.start();
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
