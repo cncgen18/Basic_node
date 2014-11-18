1c5a491b86107d4d91c25eec96edd77d44f5d81c
Michael Weyman <mweyman@combinedpublic.com>
2014-11-18 10:56:08 -0500

    fixed things

--- app.js
+++ app.js
@@ -15,18 +15,6 @@ server.route({
         reply('hello world');
     }
 });
-server.pack.register({
-    plugin: Good,
-    options: {
-        reporters: [{
-            reporter: require('good-console'),
-            args:[{ log: '*', request: '*' }]
-        }]
-    }
-}, function (err) {
-    if (err) {
-        throw err; // something bad happened loading the plugin
-    }
 // Start the server
 server.start(function () {
     console.log('Server running at:', server.info.uri);
