a5e6342c6c9a34835da804634d7a3dc71f447f0b
Michael Weyman <mweyman@combinedpublic.com>
2014-11-25 11:43:40 -0500

    finished cookie setup
    please work

--- app.js
+++ app.js
@@ -21,8 +21,64 @@ server.pack.register(require('hapi-auth-cookie'), function (err) {
         isSecure: false
     });
 
-var home = function (request,reply){
-    reply('<html><head><title>Login</title></head><body><form method="post"><p><input type="text" name="login" value="" placeholer="Username"></p><p><input type="password" name="password" value="" placeholder="password"></p><p class="submit"><input type="submit" name="commit" value="Login"></p></form></body></html>');
+//var home = function (request,reply){
+//    reply('<html><head><title>Login</title></head><body><form method="post"><p><input type="text" name="login" value="" placeholer="Username"></p><p><input type="password" name="password" value="" placeholder="password"></p><p class="submit"><input type="submit" name="commit" value="Login"></p></form></body></html>');
+//};
+
+var home = function (request, reply) {
+
+    reply('<html><head><title>Login page</title></head><body><h3>Welcome '
+      + request.auth.credentials.name
+      + '!</h3><br/><form method="get" action="/logout">'
+      + '<input type="submit" value="Logout">'
+      + '</form></body></html>');
+};
+
+var login = function (request, reply) {
+
+    if (request.auth.isAuthenticated) {
+        return reply.redirect('/');
+    }
+
+    var message = '';
+    var account = null;
+
+    if (request.method === 'post') {
+
+        if (!request.payload.username ||
+            !request.payload.password) {
+
+            message = 'Missing username or password';
+        }
+        else {
+            account = users[request.payload.username];
+            if (!account ||
+                account.password !== request.payload.password) {
+
+                message = 'Invalid username or password';
+            }
+        }
+    }
+
+    if (request.method === 'get' ||
+        message) {
+
+        return reply('<html><head><title>Login page</title></head><body>'
+            + (message ? '<h3>' + message + '</h3><br/>' : '')
+            + '<form method="post" action="/login">'
+            + 'Username: <input type="text" name="username"><br>'
+            + 'Password: <input type="password" name="password"><br/>'
+            + '<input type="submit" value="Login"></form></body></html>');
+    }
+
+    request.auth.session.set(account);
+    return reply.redirect('/');
+};
+
+var logout = function (request, reply) {
+
+    request.auth.session.clear();
+    return reply.redirect('/');
 };
 
 // Add the route
@@ -30,7 +86,7 @@ server.route({
     method: 'GET',
     path: '/',
     config: {
-        handler: home,
+        handler: Login,
         auth: 'session'
     }
 });
@@ -39,11 +95,7 @@ server.route({
     method: 'POST',
     path: '/Login',
     config: {
-        handler: function (request, reply) {
-    	   var user = request.auth.credentials.name;
-    	   var pass = request.auth.credentials.password;
-            reply('<html><head></head><body><p>Hello ' + user + '!!</p><p>Your password is : ' + pass + '</p></body></html>');
-        },
+        handler: home,
         auth: {
             mode: 'try',
             strategy: 'session'
@@ -55,6 +107,15 @@ server.route({
         }
     }
 });
+
+server.route({
+    method: 'GET',
+    path: '/logout',
+    config: {
+        handler: logout,
+        auth: 'session'
+    }
+})
 
 server.pack.register({
     plugin: Good,
