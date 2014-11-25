b8d04f2dcdba5aa877a99831050cc616281f8afe
Michael Weyman <mweyman@combinedpublic.com>
2014-11-25 11:37:49 -0500

    forgot to save on last commit

--- app.js
+++ app.js
@@ -21,22 +21,38 @@ server.pack.register(require('hapi-auth-cookie'), function (err) {
         isSecure: false
     });
 
+var home = function (request,reply){
+    reply('<html><head><title>Login</title></head><body><form method="post"><p><input type="text" name="login" value="" placeholer="Username"></p><p><input type="password" name="password" value="" placeholder="password"></p><p class="submit"><input type="submit" name="commit" value="Login"></p></form></body></html>');
+};
+
 // Add the route
 server.route({
     method: 'GET',
     path: '/',
-    handler: function (request, reply) {
-
-        reply('<html><head><title>Login</title></head><body><form method="post"><p><input type="text" name="login" value="" placeholer="Username"></p><p><input type="password" name="password" value="" placeholder="password"></p><p class="submit"><input type="submit" name="commit" value="Login"></p></form></body></html>');
+    config: {
+        handler: home,
+        auth: 'session'
     }
 });
+
 server.route({
     method: 'POST',
-    path: '/',
-    handler: function (request, reply) {
-    	var user = request.auth.credentials.name;
-    	var pass = request.auth.credentials.password;
-        reply('<html><head></head><body><p>Hello ' + user + '!!</p><p>Your password is : ' + pass + '</p></body></html>');
+    path: '/Login',
+    config: {
+        handler: function (request, reply) {
+    	   var user = request.auth.credentials.name;
+    	   var pass = request.auth.credentials.password;
+            reply('<html><head></head><body><p>Hello ' + user + '!!</p><p>Your password is : ' + pass + '</p></body></html>');
+        },
+        auth: {
+            mode: 'try',
+            strategy: 'session'
+        },
+        plugins: {
+            'hapi-auth-cookie':{
+                redirectTo: false
+            }
+        }
     }
 });
 
